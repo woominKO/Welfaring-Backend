a8f218c363ba6438e0c73f46cd098682ae85e231
woomin422@gmail.com <woomin422@gmail.com>
2025-10-28 20:12:13 +0900

    init

---
+++ src/main/java/com/demo/welfaring/WelfaringApplication.java
@@ -0,0 +1,13 @@
+package com.demo.welfaring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelfaringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelfaringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/welfaring/WelfaringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.welfaring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelfaringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
